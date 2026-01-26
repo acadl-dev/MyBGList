08381fe2b3a53b2bc6905cf59ef899c3efd01493
Ailton Costa <166161526+acadl-dev@users.noreply.github.com>
2026-01-26 02:47:27 -0300

    Regresh

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -10,22 +10,27 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(); 
-    app.UseDeveloperExceptionPage(); //estamos usando-o apenas no ambiente de desenvolvimento, para que essas informações úteis, porém potencialmente prejudiciais, estejam disponíveis somente para o desenvolvedor
+    app.UseSwaggerUI();
 }
-else { 
-    app.UseExceptionHandler("/error"); 
-}
+
+if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
+    app.UseDeveloperExceptionPage();  //estamos usando-o apenas no ambiente de desenvolvimento, para que essas informações úteis, porém potencialmente prejudiciais, estejam disponíveis somente para o desenvolvedor                                ? 
+else
+    app.UseExceptionHandler("/error");
+
+
 
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapGet("/error", () => Results.Problem());
+app.MapGet("/error", () => Results.Problem()); //Minimal API, poderia ser substituido por um controller (os Controllers são mais adequados para tarefas complexas)
+app.MapGet("/error/test", () => {throw new Exception("test"); }) ;
 
 app.MapControllers();
 
